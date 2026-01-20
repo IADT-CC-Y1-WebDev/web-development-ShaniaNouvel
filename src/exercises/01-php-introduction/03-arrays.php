adecc7a4dc9df752c7e67cb730580d856f5ef2c1
Shania Nouvel Molina <n00254662@iadt.ie>
2026-01-20 13:48:39 +0000

    Arrays Exercises Complete

--- src/exercises/01-php-introduction/03-arrays.php
+++ src/exercises/01-php-introduction/03-arrays.php
@@ -27,6 +27,13 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+        $movies = ['Tangled', 'Beauty and the Beast', 'Zootopia 2', 'Little Women', 'Elementals'];
+        echo "<ul>";
+        for ($i = 0; $i < count($movies); $i++) {
+            $movieNum = $i + 1;
+            echo "<li>Movie $movieNum: $movies[$i]</li>";
+        }
+        echo "</ul>";
         ?>
     </div>
 
@@ -42,6 +49,18 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+        $student = [
+            "name" => "Shania Nouvel Molina",
+            "studentId" => "N00254662",
+            "course" => "Creative Computing",
+            "grade" => "Perfect :D",
+        ];
+
+        $information =
+            "My name is {$student['name']}, My student number is {$student['studentId']}." . "<br>" . 
+            "I am studying {$student['course']} and my grades are {$student['grade']}.";
+
+        echo $information;
         ?>
     </div>
 
@@ -58,6 +77,20 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+        $location = [
+            "Ireland" => "Dublin",
+            "United Kingdom" => "London",
+            "France" => "Paris",
+            "Germany" => "Berlin",
+            "Italy" => "Rome"
+        ];
+
+        echo "<ul>";
+        foreach ($location as $country => $capital) {
+            echo "<li>The capital of $country is $capital.";
+        }
+        echo "</ul>";
+
         ?>
     </div>
 
@@ -75,6 +108,27 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+        $menu = [
+            'Starters' => [
+                "Smoked mackerel on toasted crumpets" => "€5.50",
+                "Sprout & herb & caper salad" => "€7.35",
+                "Creamy mushroom soup" => "€6.25"
+            ],
+            'Main Course' => [
+                "Miso Mushroom and Leek Pasta" => "€16.50",
+                "Braised Pork With Lemon, Olives and Tomatoes" => "€27.35",
+                "Meat and Potato Skillet Gratin" => "€19.25"
+            ]
+        ];
+
+        foreach ($menu as $section => $items) {
+            echo "<p>" . ucfirst($section);
+            echo "<ul>";
+            foreach ($items as $meal => $price) {
+                echo "<li>$meal\t($price)</li>";
+            }
+            echo "</ul>";
+        }
         ?>
     </div>
 
