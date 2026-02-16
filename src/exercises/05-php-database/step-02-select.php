e0307daf254b9ea0c5ebc0f862c18ac80dc6351f
Shania Nouvel Molina <n00254662@iadt.ie>
2026-02-16 12:47:07 +0000

    Databases 16/02/2026

--- src/exercises/05-php-database/step-02-select.php
+++ src/exercises/05-php-database/step-02-select.php
@@ -52,6 +52,36 @@ catch (PDOException $e) {
             // 2. Fetch all results
             // 3. Display count
             // 4. Create HTML table with the results
+            
+            try {
+                $stmt = $db->query("SELECT * FROM books ORDER BY title");
+                $books = $stmt->fetchAll();
+
+                ?>
+                <table class="data-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($books as $book): ?>
+                        <tr>
+                            <td><?= $book['id'] ?></td>
+                            <td><?= htmlspecialchars($book['title']) ?></td>
+                            <td><?= $book['author'] ?></td>
+                            <td><?= htmlspecialchars(substr($book['description'], 0, 50)) ?>...</td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>    
+                </table>
+            <?php
+            } catch (PDOException  $e) {
+                echo "Connection failes: " . $e->getMessage();
+            }
             ?>
         </div>
     </div>
